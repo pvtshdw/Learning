70ee2b10b58b877b3e6354b0d7a23c5b47845f6a
Brian Outlaw <pvtshdw@gmail.com>
2023-03-11 14:42:35 -0600

    Renamed modules folder to module

--- Terraform-Cloudflared-AWS-Tunnels/main.tf
+++ Terraform-Cloudflared-AWS-Tunnels/main.tf
@@ -1,5 +1,5 @@
 module "cloudflared-tunnel" {
-  source = "./modules/cloudflared-aws-tunnel"
+  source = "./module/cloudflared-aws-tunnel"
 }
 # terraform {
 #   required_providers {
@@ -72,39 +72,39 @@ module "cloudflared-tunnel" {
 #   proxied = true
 # }
 
-/*
- * AWS CloudflareD Server
- */
-# Generate a secure private key and encode it as a PEM
-resource "tls_private_key" "key_pair" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+# /*
+#  * AWS CloudflareD Server
+#  */
+# # Generate a secure private key and encode it as a PEM
+# resource "tls_private_key" "key_pair" {
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
 
-# Create the key pair
-resource "aws_key_pair" "demo_key_pair" {
-  key_name   = "${var.name_prefix}-key-pair"
-  public_key = tls_private_key.key_pair.public_key_openssh
-}
+# # Create the key pair
+# resource "aws_key_pair" "demo_key_pair" {
+#   key_name   = "${var.name_prefix}-key-pair"
+#   public_key = tls_private_key.key_pair.public_key_openssh
+# }
 
-# Save the file
-resource "local_file" "ssh_key" {
-  filename = "${aws_key_pair.demo_key_pair.key_name}.pem"
-  content  = tls_private_key.key_pair.private_key_pem
-}
+# # Save the file
+# resource "local_file" "ssh_key" {
+#   filename = "${aws_key_pair.demo_key_pair.key_name}.pem"
+#   content  = tls_private_key.key_pair.private_key_pem
+# }
 
-# Create the EC2 instance
-resource "aws_instance" "demo_bastion" {
-  ami           = "ami-0dfcb1ef8550277af" // AWS Linux AMI for US-East-1
-  instance_type = "t2.micro"
-  #   subnet_id = aws_subnet.qa_bastion_subnet.id
-  key_name = aws_key_pair.demo_key_pair.key_name
+# # Create the EC2 instance
+# resource "aws_instance" "demo_bastion" {
+#   ami           = "ami-0dfcb1ef8550277af" // AWS Linux AMI for US-East-1
+#   instance_type = "t2.micro"
+#   #   subnet_id = aws_subnet.qa_bastion_subnet.id
+#   key_name = aws_key_pair.demo_key_pair.key_name
 
-  user_data = templatefile("cloudflared.tftpl", { tunnel_token = cloudflare_tunnel.demo.tunnel_token })
+#   user_data = templatefile("cloudflared.tftpl", { tunnel_token = cloudflare_tunnel.demo.tunnel_token })
 
-  tags = {
-    Name          = "${var.name_prefix}-bastion"
-    "createdby"   = "marvin.martian"
-    "environment" = "prod"
-  }
-}
+#   tags = {
+#     Name          = "${var.name_prefix}-bastion"
+#     "createdby"   = "marvin.martian"
+#     "environment" = "prod"
+#   }
+# }
